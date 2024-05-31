8a888c7754f2ade814364d27633d148f5a6dfb62
takuu <takuu.tei@sun-star.co.jp>
2024-05-31 23:53:53 +0900

    更新

--- server/main.js
+++ server/main.js
@@ -1,8 +1,13 @@
 const express = require('express')
 
 const app = express();
+const port = 3002;
 
-
+app.post('/upload',(req,res) =>{
+    console.log(req);
+    res.status(200);
+    res.end('Upload');
+})
 
 app.listen(port,() => {
     console.log(`server is running on ${port}`)
