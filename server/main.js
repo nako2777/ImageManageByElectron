848d2cbf39348edd03a4e20b96acfa4783b07f61
nako2777 <zzy15963@gmail.com>
2024-06-02 23:49:30 +0900

    update

--- server/main.js
+++ server/main.js
@@ -1,9 +1,13 @@
 const express = require('express')
-
+const multer = require('multer')
+const path = require('path')
 const app = express();
+
 const port = 3002;
 
-app.post('/upload',(req,res) =>{
+const upload = multer({dest:'uploads'});
+
+app.post('/upload',upload.single('file'),(req,res) =>{
     console.log(req);
     res.status(200);
     res.end('Upload');
